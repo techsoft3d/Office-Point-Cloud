c7ae10f0e9668cbef119c45e12d80ec5262ac572
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-08-29 11:26:52 -0400

    added functionality for hc versioning

--- javascripts/common/load_scripts.js
+++ javascripts/common/load_scripts.js
@@ -1,26 +1,22 @@
-const loadScripts = () => {
-  // Load common scripts and Communicator Scripts.
-  const HWP_VERSION = "latest";
+async function loadDynamicScript() {
+  var result = await fetchVersionNumber();
+  var versionNumer = result['hcVersion']
+  var url = `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@20${versionNumer}/hoops_web_viewer.js`
 
-  // URL of files to load.
-  const files = [
-    `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@${HWP_VERSION}/hoops_web_viewer.js?v=${HWP_VERSION}`,
-    "/javascripts/communicator_scripts/web_viewer_ui.js?v=latest",
-    "/javascripts/communicator_scripts/communicator_server_integration.js?v=latest",
-    "/javascripts/communicator_scripts/sample.js?v=latest",
-  ];
-
-  files.forEach((url) => {
-    console.log("loading files.");
-    jQuery.ajax({
-      url: url,
-      dataType: "script",
-      success: () => {
-        console.log("loaded!");
-      },
-      async: true,
-    });
+  return new Promise((resolve, reject) => {
+      $.getScript(url, async function () {
+          console.log('done')
+          resolve();
+      });
   });
-};
+}
 
-loadScripts();
+async function loadIndividualScript(url) {
+  return new Promise((resolve, reject) => {
+      $.getScript(url, async function () {
+          console.log('done')
+          resolve();
+      });
+  });
+}
+
